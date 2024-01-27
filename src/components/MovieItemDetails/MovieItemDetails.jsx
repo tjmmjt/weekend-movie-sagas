50ec2b35eacb99720bbae497c4409ebf3d04168e
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-01-27 12:11:09 -0600

    added onclick() to return to MoviesList from details

--- src/components/MovieItemDetails/MovieItemDetails.jsx
+++ src/components/MovieItemDetails/MovieItemDetails.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 // TODO render MovieItemDetails
 
@@ -6,12 +7,17 @@ function MovieItemDetails() {
   // useSelector to retrieve Movie data
   const movieDetails = useSelector((store) => store.movieItemDetails);
   // console.log('Movie:', movieDetails)
-  const movieGenres = useSelector((store) => store.genres);
+  const history = useHistory()
+
+  // handle button to return to MoviesList
+  const handleClick = () => {
+    history.push('/')
+  }
 
   return movieDetails.map((movie) => (
     <div key={movie.id} className="container">
       <div className="header">
-        <button>back to movie list</button>
+        <button onClick={handleClick}>back to movie list</button>
         <h1>{movie.title}</h1>
       </div>
       <div className="movieImage">
